256cbb74d30ed30fe9cd5f0032e241bc57dfbf0e
Zineddine BEOUCHE <beouche.zineddine@gmail.com>
2020-04-04 09:45:35 +0200

    Correction de la classe TestMathematic et du main

--- td2/src/main/java/com/iut/as/StartPoint.java
+++ td2/src/main/java/com/iut/as/StartPoint.java
@@ -7,9 +7,9 @@ public class StartPoint {
 	public static void main(String[] args) {
 		System.out.println("Hello World TD2");
 		Mathematic maths = new Mathematic();
-		System.out.println("Le resultat de 2 + 4 est " + maths.addition(2, 4));
+		System.out.println("Le resultat de 2 + 4 est " + maths.addition(2d, 4d));
 		try {
-			System.out.println("Le resultat de 4 / 2 est " + maths.division(4, 2));
+			System.out.println("Le resultat de 4 / 2 est " + maths.division(4d, 2d));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
--- td2/src/test/java/com/iut/as/modele/TestMathematic.java
+++ td2/src/test/java/com/iut/as/modele/TestMathematic.java
@@ -28,74 +28,74 @@ public class TestMathematic {
 
 	@Test
 	public void testAdditionAvecNombrePositif() {
-		assertEquals(4, maths.addition(2, 2));
-		assertEquals(11, maths.addition(4, 7));
+		assertEquals(4, maths.addition(2d, 2d));
+		assertEquals(11, maths.addition(4d, 7d));
 	}
 
 	@Test
 	public void testSoustractionAvecNombrePositif() {
-		assertEquals(0, maths.soustraction(2, 2));
-		assertEquals(-3, maths.soustraction(4, 7));
+		assertEquals(0, maths.soustraction(2d, 2d));
+		assertEquals(-3, maths.soustraction(4d, 7d));
 	}
 
 	@Test
 	public void testDivisionAvecNombrePositif() {
-		assertEquals(1d, maths.division(2, 2));
-		assertEquals(2d, maths.division(8, 4));
+		assertEquals(1d, maths.division(2d, 2d));
+		assertEquals(2d, maths.division(8d, 4d));
 	}
 
 	@Test
 	public void testMultiplicationAvecNombrePositif() {
-		assertEquals(28, maths.multiplication(4, 7));
-		assertEquals(12, maths.multiplication(3, 4));
+		assertEquals(28, maths.multiplication(4d, 7d));
+		assertEquals(12, maths.multiplication(3d, 4d));
 	}
 
 	@Test
 	public void testMultiplicationAvecNombreNegatif() {
-		assertEquals(-28, maths.multiplication(-4, 7));
-		assertEquals(-12, maths.multiplication(-3, 4));
-		assertEquals(-28, maths.multiplication(4, -7));
+		assertEquals(-28, maths.multiplication(-4d, 7d));
+		assertEquals(-12, maths.multiplication(-3d, 4d));
+		assertEquals(-28, maths.multiplication(4d, -7d));
 	}
 
 	@Test
 	public void testMultiplicationAvecNombresNegatifs() {
-		assertEquals(28, maths.multiplication(-4, -7));
-		assertEquals(14, maths.multiplication(-7, -2));
+		assertEquals(28, maths.multiplication(-4d, -7d));
+		assertEquals(14, maths.multiplication(-7d, -2d));
 	}
 
 	@Test
 	public void testMultiplicationAvec0() {
-		assertEquals(0, maths.multiplication(0, -7));
-		assertEquals(0, maths.multiplication(-7, 0));
-		assertEquals(0, maths.multiplication(0, 0));
+		assertEquals(0, maths.multiplication(0d, -7d));
+		assertEquals(0, maths.multiplication(-7d, 0d));
+		assertEquals(0, maths.multiplication(0d, 0d));
 	}
 
 	@Test
 	public void testDivisionAvecNombresPositifs() {
 		// On reçoit du double :
-		assertEquals(4d, maths.division(16, 4));
-		assertEquals(4, valueOf(maths.division(8, 2)).intValue());
+		assertEquals(4d, maths.division(16d, 4d));
+		assertEquals(4, valueOf(maths.division(8d, 2d)).intValue());
 	}
 
 	@Test
 	public void testDivisionAvecNombresNegatifs() {
 		// On reçoit du double :
-		assertEquals(-4d, maths.division(-16, 4));
-		assertEquals(4d, maths.division(-16, -4));
-		assertEquals(-4, valueOf(maths.division(8, -2)).intValue());
+		assertEquals(-4d, maths.division(-16d, 4d));
+		assertEquals(4d, maths.division(-16d, -4d));
+		assertEquals(-4, valueOf(maths.division(8d, -2d)).intValue());
 	}
 
 	@Test(expected = MathematicException.class)
 	public void testDivisionParZero() {
 		// Approche plus TDD (Test Driven development)
 		// L'exception nous aide à mieux gérer ce qu'il faut faire en cas d'erreur !
-		assertEquals(-4d, maths.division(-16, 0));
+		assertEquals(-4d, maths.division(-16d, 0d));
 	}
 
 	@Test(expected = MathematicException.class)
 	public void testDivisionWithNull() {
-		assertEquals(-4d, maths.division(null, -1));
-		assertEquals(-4d, maths.division(-16, null));
+		assertEquals(-4d, maths.division(null, -1d));
+		assertEquals(-4d, maths.division(-16d, null));
 		assertEquals(-4d, maths.division(null, null));
 	}
 }
